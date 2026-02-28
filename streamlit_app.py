0bac71d21fccac106759c87341521c5829832439
Debodeep94 <debodeepbanerjee33@gmail.com>
2026-03-01 00:33:48 +0100

    adding app

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,7 @@ def get_done_uids(user):
 @st.cache_data
 def load_and_prepare_data():
     # Load your JSON file
-    with open("data.json", "r") as f:
+    with open("human_trial.json", "r") as f:
         data = json.load(f)
     
     prepared_data = []
